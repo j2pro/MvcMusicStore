ae0831b988c9d78226972abef6bba6401faad08a
James Haley <jhaley3@southwest.tn.edu>
2012-01-26 13:31:35 -0600

    initial commit

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+        //
+        // GET: /Store/Browse
+
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+
+            return message;
+        }
+
+        //
+        // GET: /Store/Details
+
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}
